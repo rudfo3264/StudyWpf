df8be2c8c134c5c8aec6da52a816d642368ff874
경래 최 <rudfo3264@naver.com>
2022-05-30 12:48:50 +0900

    220530 오전작업

---
+++ day01/WpfBasicApp/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+//alt enter
+//ctrl space
+namespace WpfBasicApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello WPF!!","First WPF");
+        }
+    }
+}
