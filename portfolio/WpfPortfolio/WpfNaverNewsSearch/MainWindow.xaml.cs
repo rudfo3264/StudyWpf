6f468172ee764b43c3584e579da55f5023e719ee
경래 최 <rudfo3264@naver.com>
2022-06-07 10:45:40 +0900

    네이버 뉴스검색 앱 마무리

--- portfolio/WpfPortfolio/WpfNaverNewsSearch/MainWindow.xaml.cs
+++ portfolio/WpfPortfolio/WpfNaverNewsSearch/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using MahApps.Metro.Controls;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -44,7 +46,7 @@ namespace WpfNaverNewsSearch
             string keyword = txtSearch.Text;
             string clientID = "5pTgIN5bfCbodvdqWEgU";
             string clientSecret = "ot73vRkU8h";
-            string openApiUri = $"https://openapi.naver.com/v1/search/news.json?start=1&display=100&query={keyword}";
+            string openApiUri = $"https://openapi.naver.com/v1/search/news.json?start={txtStartNum.Text}&display=10&query={keyword}"; //{txtStartNum.Text}
             string result;
 
             WebRequest request = null;
@@ -73,7 +75,51 @@ namespace WpfNaverNewsSearch
                 stream.Close();
                 response.Close();
             }
-            MessageBox.Show(result);
+            //MessageBox.Show(result);
+
+            var parsedJson = JObject.Parse(result);     //string to json
+
+            int total = Convert.ToInt32(parsedJson["total"]);   // 전체 검색 결과 수
+            int display = Convert.ToInt32(parsedJson["display"]);   //10
+
+            var Items = parsedJson["items"];
+            var json_array = (JArray)Items;
+
+            List<NewsItem> newsItems = new List<NewsItem>();    //데이터 그리드 연동
+            
+
+            foreach (var item in json_array)
+            {
+                var temp = DateTime.Parse(item["pubDate"].ToString());
+                NewsItem news = new NewsItem()
+                {
+                    Title = item["title"].ToString(),
+                    OriginalLink = item["originallink"].ToString(),
+                    Link = item["link"].ToString(),
+                    Description = item["description"].ToString(),
+                    //PubDate = item["pubDate"].ToString(),
+                    PubDate = temp.ToString("yyyy-MM-dd HH:mm")
+                };
+                newsItems.Add(news);
+            }
+            this.DataContext = newsItems;
         }
+
+        private void dgrResult_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            if (dgrResult.SelectedItem == null) return; // 두번째 검색부터 오류를 제거
+
+            string link = (dgrResult.SelectedItem as NewsItem).Link;
+            Process.Start(link);
+        }
+    }
+
+    internal class NewsItem
+    {
+        public string Title { get; set; }
+        public string OriginalLink { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string PubDate { get; set; }
     }
 }
