7f7576935c3da7688bfb2322244281ba0feb63c4
경래 최 <rudfo3264@naver.com>
2022-06-09 16:55:12 +0900

    20220609 SmartHome 포트폴리오 시작

--- portfolio/WpfPortfolio/DummyDataApp/Program.cs
+++ portfolio/WpfPortfolio/DummyDataApp/Program.cs
@@ -6,26 +6,69 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
 
 namespace DummyDataApp
 {
     class Program
     {
+        public static string MqttBrokerUri { get; set; }
+        public static MqttClient Client { get; set; }
+        private static Thread MqttThread { get; set; }
+        private static Faker<SensorInfo> SensorData { get; set; }
+        private static string CurrValue { get; set; }
         static void Main(string[] args)
+        {
+            InitializeConfig();     //구성초기화
+            ConnectMqttBroker();    //브로커에 접속
+            StartPublish();         //배포(Publish 발행)
+        }
+
+        private static void InitializeConfig()
+        {
+            MqttBrokerUri = "localhost";    //"127.0.0.1"; 실제 IP :  cmd창, ipconfig 입력, IPv4 주소
+
+            var Rooms = new[] { "DINNING", "LIVING", "BATH", "BED" };   //부엌, 거실, 욕실, 침실
+
+            SensorData = new Faker<SensorInfo>()
+                .RuleFor(r => r.DevId, f => f.PickRandom(Rooms))
+                .RuleFor(r => r.CurrTime, f => f.Date.Past(0))
+                .RuleFor(r => r.Temp, f => f.Random.Float(19.0f, 30.9f))
+                .RuleFor(r => r.Humid, f => f.Random.Float(40.0f, 63.9f));
+        }
+
+        private static void ConnectMqttBroker()
+        {
+            try
+            {
+                Client = new MqttClient(MqttBrokerUri);
+                Client.Connect("SMARTHOME01");
+                Console.WriteLine("접속성공");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"접속불가 : {ex}");
+                Environment.Exit(1);    //Erorr_Access_Denied
+            }
+            
+        }
+
+
+        private static void StartPublish()
+        {
+            MqttThread = new Thread(() => LoopPublish());
+            MqttThread.Start();
+        }
+
+        private static void LoopPublish()
         {
             while (true)
             {
-                var Rooms = new[] { "DINNING", "LIVING", "BATH", "BED" };   //부엌, 거실, 욕실, 침실
-                var sensorDummy = new Faker<SensorInfo>()
-                    .RuleFor(r => r.DevId, f => f.PickRandom(Rooms))
-                    .RuleFor(r => r.CurrTime, f => f.Date.Past(0))
-                    .RuleFor(r => r.Temp, f => f.Random.Float(19.0f, 30.9f))
-                    .RuleFor(r => r.Humid, f => f.Random.Float(40.0f, 63.9f));
-
-                var CuurValue = sensorDummy.Generate();
-
-                Console.WriteLine(JsonConvert.SerializeObject(CuurValue, Formatting.Indented));
-
+                SensorInfo tempValue = SensorData.Generate();
+                CurrValue = JsonConvert.SerializeObject(tempValue, Formatting.Indented);
+                Client.Publish("home/device/fakedata/", Encoding.Default.GetBytes(CurrValue));
+                Console.WriteLine($"Published : {CurrValue}");
                 Thread.Sleep(1000);
             }
         }
---
+++ portfolio/WpfPortfolio/WpfSmartHomeMonitoringApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,80 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfSmartHomeMonitoringApp.Helpers;
+
+namespace WpfSmartHomeMonitoringApp.ViewModels
+{
+    public class MainViewModel : Conductor<object>  // Screen에는 ActivateItemAsync메서드가 존재하지 않음
+    {
+        public MainViewModel()
+        {
+            DisplayName = "SmartHome Monitoring v2.0";  //윈도우 타이틀, 제목
+        }
+
+        //TODO
+        public void LoadDataBaseView()
+        {
+            //if (Commons.MQTT_CLIENT != null)
+                ActivateItemAsync(new DataBaseViewModel());
+            //else
+            //    var windowManager = new WindowManager();
+            //windowManager.ShowDialog(new ErrorPopupViewModel("Report|MQTT doesn't start, yet"));
+        }
+        public void LoadRealTimeView()
+        {
+            ActivateItemAsync(new RealTimeViewModel());
+        }
+
+        public void LoadHistoryView()
+        {
+            ActivateItemAsync(new HistoryViewModel());
+        }
+
+
+        //protected override void OnDeactivateAsync(bool close)
+        //{
+        //    if(Commons.MQTT_CLIENT.IsConnected)
+        //    {
+        //        Commons.MQTT_CLIENT.Disconnect();
+        //        Commons.MQTT_CLIENT = null;
+        //    }
+
+        //    base.OnDeactivateAsync(close);
+        //}
+
+        //public async void PopInfoDialog(object o)
+        //{
+        //    await TaskStart();
+        //}
+        //public void StartSubscriber()
+        //{
+        //    TaskStart();
+        //}
+
+        //private void TaskStart()
+        //{
+        //    var windowManager = new WindowManager();
+        //    var result = windowManager.ShowDialog(new CustomPipupViewModel("New Network"));
+
+        //    if(result == true)
+        //    {
+        //        ActivateItemAsync(new DatabaseMonitoringViewModel());
+        //    }
+        //}
+
+
+
+
+
+
+        public void ExitProgram()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
