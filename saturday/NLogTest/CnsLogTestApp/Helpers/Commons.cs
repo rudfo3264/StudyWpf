9c16a556d702f7cf28fcc0df0e33b7f978b13adf
경래 최 <rudfo3264@naver.com>
2022-06-11 15:47:11 +0900

    20220611 토요일 NLog 학습

---
+++ saturday/NLogTest/CnsLogTestApp/Helpers/Commons.cs
@@ -0,0 +1,15 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnsLogTestApp
+{
+    class Commons
+    {
+        //NLog용 정적(Static) 인스턴스 생성
+        public static readonly Logger LOGGER = LogManager.GetLogger("Commons");
+    }
+}
